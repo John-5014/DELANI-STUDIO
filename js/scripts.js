669627067b3fbb8d00c0869a670b95d9c817f270
John-5014 <kaiserjohn52@gmail.com>
2019-08-25 11:34:28 +0300

    add effects to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+  $("#des").click(function() {
+   $('.img1').slideToggle(2800);
+    $(".p1").slideToggle();
+  });
+  $("#dev").click(function() {
+    $(".p2, .img2").slideToggle(2800);
+  });
+  $("#products").click(function() {
+    $(".p3, .img3").slideToggle(2800);
+  });
+
+  $('#hoVer').hover(function() {
+    $(this).children(".overlay").fadeToggle(1000, "linear");
+    $(this).toggle(css('.'))
+    $(".write").toggle();
+    $('.line').showToggle();
+
+   
+});
+$('.line').hover(function() {
+  $(this).animate({
+    opacity: '1'
+  });
+},
+function() {
+  $(this).animate({
+    opacity: '0'
+  });
+});
+
+  $('.line').hover(function(){
+  	$(this).animate({
+  		opacity: '0'
+  	})
+  })
+
+
+
+  $("input").click(function(event) {
+    event.preventDefault();
+    var user = document.getElementById('input1').value;
+    alert(user + "Hi! we've received your message, thank you for getting in touch with us");
+    
+
+
+
+  });
+
+
+
+});
+
+
+
